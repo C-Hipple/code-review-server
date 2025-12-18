2a7fb534ecc7cb9467dbd3acd0829661f6093f65
Chris Hipple <chrishipple@multimediallc.com>
2025-12-18 14:46:32 -0500

    add team

--- git_tools/git_tools.go
+++ git_tools/git_tools.go
@@ -187,7 +187,7 @@ func MakeTeamFilters(teams []string) func([]*github.PullRequest) []*github.PullR
 }
 
 func FilterMyTeamRequested(prs []*github.PullRequest) []*github.PullRequest {
-	teams := []string{"growth-pod-review", "purchase-pod-review", "growth-and-purchase-pod", "coreteam-review", "chat-pod-review-backend", "creator-team", "affiliate-program-experts", "email-experts"}
+	teams := []string{"growth-pod-review", "purchase-pod-review", "growth-and-purchase-pod", "coreteam-review", "chat-pod-review-backend", "creator-team", "affiliate-program-experts", "email-experts", "identity-experts"}
 	filtered := []*github.PullRequest{}
 	for _, pr := range prs {
 		for _, team := range pr.RequestedTeams {

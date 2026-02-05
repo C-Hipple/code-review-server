a2b56aecaf08900f9939519096319bd2592ec36d
chris <chris.hipple@gmail.com>
2026-02-04 21:35:05 -0500

    more tests

--- git_tools/waiting_on_me_test.go
+++ git_tools/waiting_on_me_test.go
@@ -90,6 +90,25 @@ func TestFilterWaitingOnMe(t *testing.T) {
 			shouldInclude: false,
 		},
 		{
+			name: "Personally requested, never acted (fresh request)",
+			pr:   makePR(6, myLogin),
+			state: InteractionState{
+				LastCommitTime: time.Now().Add(-30 * time.Minute),
+			},
+			shouldInclude: true,
+		},
+		{
+			name: "Personally requested and has unresponded comments",
+			pr:   makePR(7, myLogin),
+			state: InteractionState{
+				LastMeTime:             time.Now().Add(-10 * time.Minute),
+				LastOthersTime:         time.Now().Add(-20 * time.Minute),
+				LastCommitTime:         time.Now().Add(-30 * time.Minute),
+				HasUnrespondedComments: true,
+			},
+			shouldInclude: true,
+		},
+		{
 			name:          "Nil PR should be ignored",
 			pr:            nil,
 			shouldInclude: false,
@@ -206,6 +225,101 @@ func TestCalculateInteractionState(t *testing.T) {
 			},
 		},
 		{
+			name: "Issue comments - I replied last",
+			pr:   &github.PullRequest{},
+			issueComments: []*github.IssueComment{
+				makeIssueComment(other, now.Add(-20 * time.Minute)),
+				makeIssueComment(myLogin, now.Add(-10 * time.Minute)),
+			},
+			expectedState: InteractionState{
+				LastMeTime:             now.Add(-10 * time.Minute),
+				LastOthersTime:         now.Add(-20 * time.Minute),
+				HasUnrespondedComments: false,
+			},
+		},
+		{
+			name: "Issue comments - only others, I never participated",
+			pr:   &github.PullRequest{},
+			issueComments: []*github.IssueComment{
+				makeIssueComment(other, now.Add(-10 * time.Minute)),
+			},
+			expectedState: InteractionState{
+				LastOthersTime:         now.Add(-10 * time.Minute),
+				HasUnrespondedComments: false,
+			},
+		},
+		{
+			name: "Review comment thread where I did not participate",
+			pr:   &github.PullRequest{},
+			reviewComments: []*github.PullRequestComment{
+				makeComment(10, other, now.Add(-20 * time.Minute), 0),
+				makeComment(11, "third", now.Add(-10 * time.Minute), 10),
+			},
+			expectedState: InteractionState{
+				LastOthersTime:         now.Add(-10 * time.Minute),
+				HasUnrespondedComments: false,
+			},
+		},
+		{
+			name: "Multiple review comment threads - one resolved, one not",
+			pr:   &github.PullRequest{},
+			reviewComments: []*github.PullRequestComment{
+				makeComment(20, myLogin, now.Add(-30 * time.Minute), 0),
+				makeComment(21, other, now.Add(-25 * time.Minute), 20),
+				makeComment(22, myLogin, now.Add(-20 * time.Minute), 20),
+				makeComment(23, myLogin, now.Add(-15 * time.Minute), 0),
+				makeComment(24, other, now.Add(-10 * time.Minute), 23),
+			},
+			expectedState: InteractionState{
+				LastMeTime:             now.Add(-15 * time.Minute),
+				LastOthersTime:         now.Add(-10 * time.Minute),
+				HasUnrespondedComments: true,
+			},
+		},
+		{
+			name: "Mixed sources - all resolved",
+			pr:   &github.PullRequest{},
+			reviews: []*github.PullRequestReview{
+				makeReview(other, now.Add(-40 * time.Minute)),
+				makeReview(myLogin, now.Add(-35 * time.Minute)),
+			},
+			reviewComments: []*github.PullRequestComment{
+				makeComment(30, myLogin, now.Add(-30 * time.Minute), 0),
+				makeComment(31, other, now.Add(-25 * time.Minute), 30),
+				makeComment(32, myLogin, now.Add(-20 * time.Minute), 30),
+			},
+			issueComments: []*github.IssueComment{
+				makeIssueComment(other, now.Add(-15 * time.Minute)),
+				makeIssueComment(myLogin, now.Add(-10 * time.Minute)),
+			},
+			expectedState: InteractionState{
+				LastMeTime:             now.Add(-10 * time.Minute),
+				LastOthersTime:         now.Add(-15 * time.Minute),
+				HasUnrespondedComments: false,
+			},
+		},
+		{
+			name: "Mixed sources - issue comment unresolved",
+			pr:   &github.PullRequest{},
+			reviews: []*github.PullRequestReview{
+				makeReview(myLogin, now.Add(-40 * time.Minute)),
+			},
+			reviewComments: []*github.PullRequestComment{
+				makeComment(40, myLogin, now.Add(-30 * time.Minute), 0),
+				makeComment(41, other, now.Add(-25 * time.Minute), 40),
+				makeComment(42, myLogin, now.Add(-20 * time.Minute), 40),
+			},
+			issueComments: []*github.IssueComment{
+				makeIssueComment(myLogin, now.Add(-15 * time.Minute)),
+				makeIssueComment(other, now.Add(-10 * time.Minute)),
+			},
+			expectedState: InteractionState{
+				LastMeTime:             now.Add(-15 * time.Minute),
+				LastOthersTime:         now.Add(-10 * time.Minute),
+				HasUnrespondedComments: true,
+			},
+		},
+		{
 			name: "Nil checks (should not panic)",
 			pr:   nil,
 			reviews: []*github.PullRequestReview{
@@ -236,6 +350,102 @@ func TestCalculateInteractionState(t *testing.T) {
 			}
 			if state.HasUnrespondedComments != tt.expectedState.HasUnrespondedComments {
 				t.Errorf("HasUnrespondedComments: expected %v, got %v", tt.expectedState.HasUnrespondedComments, state.HasUnrespondedComments)
+			}
+		})
+	}
+}
+
+func TestFilterWaitingOnAuthor(t *testing.T) {
+	cfg := config.C()
+	cfg.GithubUsername = "myself"
+	config.SetC(cfg)
+
+	owner := "owner"
+	repo := "repo"
+
+	makePR := func(number int) *github.PullRequest {
+		return &github.PullRequest{
+			Number: &number,
+			User:   &github.User{Login: github.String("author")},
+			Base: &github.PullRequestBranch{
+				Repo: &github.Repository{
+					Owner: &github.User{Login: &owner},
+					Name:  &repo,
+				},
+			},
+		}
+	}
+
+	tests := []struct {
+		name          string
+		pr            *github.PullRequest
+		state         InteractionState
+		shouldInclude bool
+	}{
+		{
+			name: "I acted after others and after commit",
+			pr:   makePR(100),
+			state: InteractionState{
+				LastMeTime:     time.Now().Add(-5 * time.Minute),
+				LastOthersTime: time.Now().Add(-10 * time.Minute),
+				LastCommitTime: time.Now().Add(-20 * time.Minute),
+			},
+			shouldInclude: true,
+		},
+		{
+			name: "Others acted after me",
+			pr:   makePR(101),
+			state: InteractionState{
+				LastMeTime:     time.Now().Add(-20 * time.Minute),
+				LastOthersTime: time.Now().Add(-5 * time.Minute),
+				LastCommitTime: time.Now().Add(-10 * time.Minute),
+			},
+			shouldInclude: false,
+		},
+		{
+			name: "New commit after my action",
+			pr:   makePR(102),
+			state: InteractionState{
+				LastMeTime:     time.Now().Add(-10 * time.Minute),
+				LastOthersTime: time.Now().Add(-20 * time.Minute),
+				LastCommitTime: time.Now().Add(-5 * time.Minute),
+			},
+			shouldInclude: false,
+		},
+		{
+			name: "Never acted (LastMeTime zero)",
+			pr:   makePR(103),
+			state: InteractionState{
+				LastOthersTime: time.Now().Add(-10 * time.Minute),
+				LastCommitTime: time.Now().Add(-5 * time.Minute),
+			},
+			shouldInclude: false,
+		},
+		{
+			name:          "All times zero",
+			pr:            makePR(104),
+			state:         InteractionState{},
+			shouldInclude: false,
+		},
+		{
+			name:          "Nil PR ignored",
+			pr:            nil,
+			shouldInclude: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.pr != nil {
+				cacheKey := fmt.Sprintf("interaction_state:%s/%s:%d", owner, repo, *tt.pr.Number)
+				GlobalCache.Set(cacheKey, tt.state, 1*time.Hour)
+			}
+
+			result := FilterWaitingOnAuthor([]*github.PullRequest{tt.pr})
+			included := len(result) > 0
+
+			if included != tt.shouldInclude {
+				t.Errorf("expected included=%v, got %v", tt.shouldInclude, included)
 			}
 		})
 	}

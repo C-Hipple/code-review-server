af62f9cfb5c321dc18fbe6668b45ba25b8940811
chris <chris.hipple@gmail.com>
2026-02-05 19:46:21 -0500

    yup

---
+++ bun_client/server.test.ts
@@ -0,0 +1,7 @@
+import { expect, test, describe } from 'bun:test';
+
+describe('Server', () => {
+    test('placeholder test - remove when real tests added', () => {
+        expect(1 + 1).toBe(2);
+    });
+});

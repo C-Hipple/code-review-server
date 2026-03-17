ed318d2f45585c7d8f7f74447d2dffb8e48b9d6e
Chris Hipple <C-Hipple@users.noreply.github.com>
2026-03-17 11:26:37 -0400

    paren (#109)
    * paren
    
    * state

--- client.el/client.el
+++ client.el/client.el
@@ -877,13 +877,13 @@ SHOW-FULL-COMMENTS determines whether to show full content or indicators."
       (setq sb (concat sb (format "Refs:  %s ... %s\n" base head)))
       (setq sb (concat sb (format "URL:   %s\n" url)))
       (setq sb (concat sb (format "State: \t%s\n" state)))
-      (setq sb (concat sb (format "Milestone: \t%s\n" (or milestone "No milestone"))))
+      (setq sb (concat sb (format "Draft: \t%s\n" (if (eq draft t) "True" "False"))))
+      ;; (setq sb (concat sb (format "Milestone: \t%s\n" (or milestone "No milestone"))))
 
       (let ((labels-str (if (> (length labels) 0) (string-join (append labels nil) ", ") "None yet")))
         (setq sb (concat sb (format "Labels: \t%s\n" labels-str))))
 
       (setq sb (concat sb "Projects: \tNone yet\n"))
-      (setq sb (concat sb (format "Draft: \t%s\n" (if (eq draft t) "true" "false"))))
 
       (let ((assignees-str (if (> (length assignees) 0) (string-join (append assignees nil) ", ") "No one -- Assign yourself")))
         (setq sb (concat sb (format "Assignees: \t%s\n" assignees-str))))
@@ -1281,8 +1281,9 @@ If PREVIOUS is non-nil, navigate to the previous PR; otherwise navigate to the n
            (let* ((metadata (cdr (assq 'metadata result)))
                   (url (cdr (assq 'url metadata)))
                   (pr-info (when (and url (string-match
-                                         "github\\.com/\\([^/]+\\)/\\([^/]+\\)/pull/\\([0-9]+\\)"
-                                         url))
+
+                                           "github\\.com/\\([^/]+\\)/\\([^/]+\\)/pull/\\([0-9]+\\)"
+                                           url))
                              (list (match-string 1 url)
                                    (match-string 2 url)
                                    (string-to-number (match-string 3 url)))))
@@ -2112,7 +2113,7 @@ Uses cached data from the general plugin output buffer if available."
                    (setq crs--plugin-number number)
                    (setq crs--plugin-name plugin)))
                (pop-to-buffer buffer)
-               (message "Plugin output loaded."))))))))
+               (message "Plugin output loaded.")))))))))
 
 (defun crs-run-on-demand-plugin ()
   "Run an on-demand plugin for the current PR.
@@ -2155,6 +2156,7 @@ BRANCH-NAME is the name of the branch to checkout."
       (shell-command (concat "git fetch && git checkout " branch-name)))
     (unless (file-directory-p project-dir)
       (error "Project directory %s not found" project-dir))))
+
 
 
 (defun crs--get-ref-name ()
